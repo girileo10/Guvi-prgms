4e9704be1e5dca6ddff4baf6c0057632f0478b22
Giri <44906019+girileo10@users.noreply.github.com>
2020-05-14 13:47:18 +0530

    Create days_in_month.cpp

---
+++ days_in_month.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int ch;
+  cin >> ch;
+  switch(ch)
+  {
+    case 1:
+      cout << "31";
+      break;
+    case 2:
+      cout << "28";
+      break;
+    case 3:
+      cout << "31";
+      break;
+    case 4:
+      cout << "30";
+      break;
+    case 5:
+      cout << "31";
+      break;
+    case 6:
+      cout << "30";
+      break;
+    case 7:
+      cout << "31";
+      break;
+    case 8:
+      cout << "31";
+      break;
+    case 9:
+      cout << "30";
+      break;
+    case 10:
+      cout << "31";
+      break;
+    case 11:
+      cout << "30";
+      break;
+    case 12:
+      cout << "31";
+      break;
+    default:
+      cout << "Error";
+      break;
+  }
+      
+    //cout << "Hello World" << endl;
+    return 0;
+}
